8aa30054299783f89f85de06b1a14eea22b2f756
lavirlifiliol <adam.hrbac@gmail.com>
2020-05-15 19:07:50 +0200

    fix things

--- src/game/logic.scm
+++
@@ -1,7 +0,0 @@
-(define-library
-  (game logic)
-  (export text)
-  (import (scheme base))
-  (begin
-    (define (text)
-      "Hello world!")))
--- test/game/logic.scm
+++
@@ -1,9 +0,0 @@
-(import (srfi 64))
-(import (game logic))
-
-(test-begin "Test gamelogic")
-
-(test-group ""
-            (test-assert (equal? "Hello world!" (text))))
-
-(test-end "Test gamelogic")
